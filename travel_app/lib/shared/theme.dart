d806cc79b8050839b6c250bd8c309be24ff13492
Gilang Martadinata <50565813+geemde@users.noreply.github.com>
2022-01-23 14:18:12 +0700

    shared color and google fonts

---
+++ travel_app/lib/shared/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = const Color(0xff5C40CC);
+Color kBlackColor = const Color(0xff1F1449);
+Color kWhiteColor = const Color(0xffFFFFFF);
+Color kGreyColor = const Color(0xff9698A9);
+Color kGreenColor = const Color(0xff0EC3AE);
+Color kRedColor = const Color(0xffEB70A5);
+Color kBackgroundColor = const Color(0xffFAFAFA);
+Color kInactiveColor = const Color(0xffDBD7EC);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor
+);
+
+
